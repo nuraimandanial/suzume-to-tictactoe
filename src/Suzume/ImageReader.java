230561c7d0f6b11e5ec8b3ccaae727bebdfa205f
kinderBono <nuraimandanialmohdzaki@gmail.com>
2023-05-06 05:26:57 +0800

    Update ImageReader.java
    Updated for interactive file directory selector.

--- src/Suzume/ImageReader.java
+++ src/Suzume/ImageReader.java
@@ -6,29 +6,38 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 
 public class ImageReader {
     public static void main(String[] args) throws IOException {
-        for (int i = 1; i <= 4; i++) {
-            File imagePath = new File("C:\\Users\\User\\OneDrive\\Documents\\UNIMALAYA\\SEMESTER 2\\WIA1002 - DATA STRUCTURE\\ASSIGNMENT\\Pieces of Map\\Pieces of Map\\image " + i + ".png");
-            System.out.println(imagePath);
-            BufferedImage image = ImageIO.read(imagePath);
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+        chooser.setDialogTitle("Select Directory");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int result = chooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File directory = chooser.getSelectedFile();
+            
+            for (int i = 1; i <= 4; i++) {
+                File imagePath = new File(directory, "image " + i + ".png");
+                BufferedImage image = ImageIO.read(imagePath);
 
-            int width = image.getWidth();
-            int height = image.getHeight();
-            int[][] bitData = new int[width][height];
+                int width = image.getWidth();
+                int height = image.getHeight();
+                int[][] bitData = new int[width][height];
 
-            for (int j = 0; j < width; j++) {
-                for (int k = 0; k < height; k++) {
-                    int pixel = image.getRGB(j, k);
-                    int grey = (pixel >> 16) & 0xff;
-                    bitData[j][k] = grey;
+                for (int j = 0; j < width; j++) {
+                    for (int k = 0; k < height; k++) {
+                        int pixel = image.getRGB(j, k);
+                        int grey = (pixel >> 16) & 0xff;
+                        bitData[j][k] = grey;
+                    }
                 }
-            }
 
-            System.out.println("Image " + i);
-            for (int j = 1; j < bitData.length; j++) {
-                System.out.println(Arrays.toString(bitData[j]));
+                System.out.println("Image " + i);
+                for (int j = 1; j < bitData.length; j++) {
+                    System.out.println(Arrays.toString(bitData[j]));
+                }
             }
         }
     }
