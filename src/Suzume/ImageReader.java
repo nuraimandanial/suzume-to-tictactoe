f0484abc675677dc8870226e761f23f6854c1827
kinderBono <nuraimandanialmohdzaki@gmail.com>
2023-05-06 05:59:49 +0800

    Completed Image Scaling.
    Scaled down the bit size into values of 0 - 3 for all Image Map.

--- src/Suzume/ImageReader.java
+++ src/Suzume/ImageReader.java
@@ -9,7 +9,39 @@ import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 
 public class ImageReader {
+    private int[][] bitData;
+
+    public int[][] imageReader(File directory, int i) throws IOException { // Actually can just directly scale down the bits value into range of 0 - 3, but lets make it longer process.
+        File imagePath = new File(directory, "image " + i + ".png");
+        BufferedImage image = ImageIO.read(imagePath);
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+        bitData = new int[width][height];
+
+        for (int j = 0; j < width; j++) {
+            for (int k = 0; k < height; k++) {
+                int pixel = image.getRGB(j, k);
+                int grey = (pixel >> 16) & 0xff;
+                bitData[j][k] = grey;
+            }
+        }
+
+        return bitData;
+    }
+
+    public int[][] scaleDown(int[][] bitData) {
+        for (int i = 0; i < bitData.length; i++) {
+            for (int j = 0; j < bitData[i].length; j++) {
+                bitData[i][j] = bitData[i][j] / 64;
+            }
+        }
+
+        return bitData;
+    }
+
     public static void main(String[] args) throws IOException {
+        
         JFileChooser chooser = new JFileChooser();
         chooser.setCurrentDirectory(new File("."));
         chooser.setDialogTitle("Select Directory");
@@ -19,24 +51,14 @@ public class ImageReader {
             File directory = chooser.getSelectedFile();
             
             for (int i = 1; i <= 4; i++) {
-                File imagePath = new File(directory, "image " + i + ".png");
-                BufferedImage image = ImageIO.read(imagePath);
-
-                int width = image.getWidth();
-                int height = image.getHeight();
-                int[][] bitData = new int[width][height];
-
-                for (int j = 0; j < width; j++) {
-                    for (int k = 0; k < height; k++) {
-                        int pixel = image.getRGB(j, k);
-                        int grey = (pixel >> 16) & 0xff;
-                        bitData[j][k] = grey;
+                int[][] map = new ImageReader().scaleDown(new ImageReader().imageReader(directory, i));   
+                
+                System.out.println("\nImage " + i);
+                for (int j = 0; j < map.length; j++) {
+                    for (int k = 0; k < map[j].length; k++) {
+                        System.out.printf("%3d,", map[j][k]);
                     }
-                }
-
-                System.out.println("Image " + i);
-                for (int j = 1; j < bitData.length; j++) {
-                    System.out.println(Arrays.toString(bitData[j]));
+                    System.out.println();
                 }
             }
         }
