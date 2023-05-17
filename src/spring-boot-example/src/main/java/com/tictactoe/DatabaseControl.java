14e4060c0b957a14ca55aa7f083da05e81d70249
Ng Ching Wei <22004844@siswa.um.edu.my>
2023-05-17 22:19:35 +0800

    Tic Tac Toe

--- src/spring-boot-example/src/main/java/com/tictactoe/DatabaseControl.java
+++ src/spring-boot-example/src/main/java/com/tictactoe/DatabaseControl.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
